2afc9e4efc2e525187b72c8bfeca1fbba3b734b9
rafael oshikawa <rafael.oshikawa@hotmail.com[>
2024-03-20 23:35:02 -0300

    commit initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import Header from './components/Cabecalho'
 import Hero from './components/Hero'
-import ListaVagas from './containers/ListaVagas'
+import ListaVagas from './components/ListaVagas'
 
 import './global.css'
 
